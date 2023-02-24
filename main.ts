1b66c775e2d13bbcc46053a79030dd8f02173390
zdkderpphb <126237577+zdkderpphb@users.noreply.github.com>
2023-02-24 13:46:36 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ load dependency
 */
 
 
-//% color="#31C7D5" weight=10 icon="\uf1d0"
+//% color="#63AEE6" weight=10 icon="\f19d"
 namespace Robobox {
     const PCA9685_ADDRESS = 0x40
     const MODE1 = 0x00
